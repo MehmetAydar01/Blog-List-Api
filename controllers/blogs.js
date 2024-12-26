13c8e3dde93462b892e725fdef483745dd31fd21
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-12-27 01:32:20 +0300

    4.8-4.12 alistirmalari cozuldu ve kod asenkron yapıya donusturuldu

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,21 +1,22 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response, next) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
 
-  blog
-    .save()
-    .then((savedBlog) => {
-      response.status(201).json(savedBlog)
-    })
-    .catch((error) => next(error))
+  const blogObj = {
+    ...body,
+    likes: body.likes || 0,
+  }
+
+  const blog = new Blog(blogObj)
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
